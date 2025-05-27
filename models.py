52fb9c883b97a1f989a94589450c50de8d6cf663
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2025-05-27 09:20:24 +0300

    create categories table

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, Integer, Text
+from sqlalchemy import Column, Integer, Text, ForeignKey, DateTime
+from datetime import datetime
 
 # Create a base class
 class Base(DeclarativeBase):
@@ -9,11 +10,22 @@ class Base(DeclarativeBase):
 1. We must provide the table via the __tablename__ attribute
 2. It must have at least one column defined
 """
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(Text, unique = True)
+    created_at = Column(DateTime, default = datetime.now())
+
 class Product(Base):
     __tablename__ = "products"
 
     id = Column(Integer(), primary_key = True)
     name = Column(Text())
     price = Column(Integer())
+    image = Column(Text)
+    category_id = Column(Integer(), ForeignKey("categories.id"))
+    created_at = Column(DateTime, default = datetime.now())
+
 
 
